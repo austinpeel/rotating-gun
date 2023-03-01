789346f12e981ce2c899f202c826d90e66375f98
Austin Peel <austinpeel@gmail.com>
2023-03-01 13:10:41 +0100

    Animate theta UI

---
+++ Assets/Sections/Theory/Scripts/AnimatedTheta.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedTheta : MonoBehaviour
+{
+    public SimulationState simState;
+    public Image circleImage;
+    public Image thetaLabel;
+    public float thetaRadius = 30;
+
+    private SimulationState.ReferenceFrame currentReferenceFrame;
+
+    private void LateUpdate()
+    {
+        if (!simState) return;
+
+        if (circleImage)
+        {
+            float fillAmount = simState.theta / 360f;
+            if (simState.referenceFrame == SimulationState.ReferenceFrame.Gun)
+            {
+                fillAmount = 1 - fillAmount;
+            }
+            circleImage.fillAmount = fillAmount;
+        }
+
+        if (thetaLabel)
+        {
+            float angle = simState.theta * Mathf.Deg2Rad;
+            if (simState.referenceFrame == SimulationState.ReferenceFrame.Gun)
+            {
+                angle = 2 * Mathf.PI - angle;
+            }
+            angle *= 0.5f;
+            Vector2 direction = Mathf.Cos(angle) * Vector2.right + Mathf.Sin(angle) * Vector2.up;
+            thetaLabel.rectTransform.anchoredPosition = thetaRadius * direction;
+        }
+    }
+}
