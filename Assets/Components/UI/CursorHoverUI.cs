f9d48a2c82ee52b5902196796378350c492e70b2
Austin Peel <austinpeel@gmail.com>
2023-03-08 19:25:39 +0100

    Update UI elements to use the custom cursor SO

--- Assets/Components/UI/CursorHoverUI.cs
+++ Assets/Components/UI/CursorHoverUI.cs
@@ -5,21 +5,12 @@ public class CursorHoverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 {
     [SerializeField] private CustomCursor customCursor;
 
-    [SerializeField] private Texture2D hoverCursor = null;
-    [SerializeField] private Vector2 hotspot = default;
-
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // Display the cursor while hovering
         if (customCursor)
         {
             Cursor.SetCursor(customCursor.texture, customCursor.hotspot, CursorMode.Auto);
-            return;
-        }
-
-        // Display the cursor while hovering
-        if (hoverCursor != null)
-        {
-            Cursor.SetCursor(hoverCursor, hotspot, CursorMode.Auto);
         }
     }
 
@@ -30,14 +21,8 @@ public class CursorHoverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
     private void RestoreDefault()
     {
-        if (customCursor)
-        {
-            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-            return;
-        }
-
         // Restore the default cursor
-        if (hoverCursor != null)
+        if (customCursor)
         {
             Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         }
