98ef38e89baa59a51a282157647484fee71dd81b
Austin Peel <austinpeel@gmail.com>
2023-03-09 13:08:29 +0100

    Update theory vectors and prefabs

--- Assets/Simulation/Gun/Bullet.cs
+++ Assets/Simulation/Gun/Bullet.cs
@@ -3,44 +3,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject positionVectorPrefab;
-    // public GameObject velocityVectorPrefab;
+    // public GameObject positionVectorPrefab;
     public float maxDistance = 10;
 
     public static event Action<Bullet> OnOutOfBounds;
 
-    // Kinematics
     public Vector3 Position => transform.position;
 
     private Vector3 velocity;
     public Vector3 Velocity => velocity;
-
-    // private Vector positionVector;
 
     public void Initialize(Vector3 position, Vector3 velocity, float maxDistance)
     {
         transform.position = position;
         this.velocity = velocity;
         this.maxDistance = maxDistance;
-
-        // if (showPositionVector)
-        // {
-        //     positionVector = Instantiate(positionVectorPrefab, Vector3.zero, Quaternion.identity, transform.parent).GetComponent<Vector>();
-        //     positionVector.components = transform.position;
-        //     positionVector.Redraw();
-        // }
     }
 
     private void Update()
     {
         transform.Translate(velocity * Time.deltaTime, Space.World);
 
-        // if (positionVector)
-        // {
-        //     positionVector.components = transform.position - 0.5f * transform.localScale.x * transform.position.normalized;
-        //     positionVector.Redraw();
-        // }
+        CheckForOutOfBounds();
+    }
 
+    public void CheckForOutOfBounds()
+    {
         if (transform.position.magnitude >= maxDistance)
         {
             OnOutOfBounds?.Invoke(this);
@@ -50,11 +38,5 @@ public class Bullet : MonoBehaviour
     public void Destroy()
     {
         Destroy(gameObject);
-        // DestroyPositionVector();
     }
-
-    // public void DestroyPositionVector()
-    // {
-    //     if (positionVector) Destroy(positionVector.gameObject);
-    // }
 }
