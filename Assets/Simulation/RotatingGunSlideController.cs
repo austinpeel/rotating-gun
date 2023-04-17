3b096cc3fbfdf9b75ce0c58e52e81ca9e507e863
Austin Peel <austinpeel@gmail.com>
2023-04-17 18:26:16 +0200

    Fix camera movement in introduction

--- Assets/Simulation/RotatingGunSlideController.cs
+++ Assets/Simulation/RotatingGunSlideController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotatingGunSlideController : Slides.SimulationSlideController
 {
@@ -7,15 +8,14 @@ public class RotatingGunSlideController : Slides.SimulationSlideController
     public bool traceBulletPath;
     public bool showPosition;
 
+    [Header("Buttons")]
+    public Button fireButton;
+
     [Header("Lights")]
     public GameObject labFrameLight;
     public bool activateLabFrameLight;
     public GameObject gunFrameLight;
     public bool activateGunFrameLight;
-
-    [Header("Inset Camera")]
-    public Camera insetCamera;
-    public bool showInsetCamera;
 
     [Header("Ground")]
     public Transform ground;
@@ -27,21 +27,30 @@ public class RotatingGunSlideController : Slides.SimulationSlideController
     private Vector3 cameraPosition;
     private Quaternion cameraRotation;
 
+    private void Awake()
+    {
+        sim = simulation as RotatingGunSimulation;
+    }
+
     public override void InitializeSlide()
     {
-        Debug.Log("Initializing " + transform.name);
+        // Debug.Log("Initializing " + transform.name);
 
         sim.angularFrequency = angularFrequency;
         sim.SetGunOmegaFromAngularFrequency(angularFrequency);
         sim.referenceFrame = referenceFrame;
         sim.traceBulletPath = traceBulletPath;
         sim.showPosition = showPosition;
+
+        // Reset the positions and orientations of the camera and simulation
+        sim.Reset(cameraPosition, cameraRotation);
+
         sim.Pause();
 
         if (labFrameLight) labFrameLight.SetActive(activateLabFrameLight);
         if (gunFrameLight) gunFrameLight.SetActive(activateGunFrameLight);
-        if (insetCamera) insetCamera.gameObject.SetActive(false);
         if (ground) ground.gameObject.SetActive(showGround);
+        if (fireButton) fireButton.interactable = false;
 
         slideHasInitialized = true;
     }
@@ -49,46 +58,29 @@ public class RotatingGunSlideController : Slides.SimulationSlideController
     private void OnEnable()
     {
         CameraController.OnCameraMovementComplete += HandleCameraMovementComplete;
-
-        sim = simulation as RotatingGunSimulation;
     }
 
     private void OnDisable()
     {
         CameraController.OnCameraMovementComplete -= HandleCameraMovementComplete;
-
-        // Reset the positions and orientations of the camera and simulation
-        if (slideHasInitialized) sim.Reset(cameraPosition, cameraRotation);
     }
 
     public void HandleCameraMovementComplete(Vector3 cameraPosition, Quaternion cameraRotation)
     {
-        // Debug.Log(transform.name + " sim is not null ? " + (sim != null));
-
-        // if (enabled)
-        // {
-        //     this.cameraPosition = cameraPosition;
-        //     this.cameraRotation = cameraRotation;
-
-        //     if (sim) sim.Resume();
-
-        //     if (insetCamera) insetCamera.gameObject.SetActive(showInsetCamera);
-        // }
-
         if (slideHasInitialized)
         {
-            Debug.Log(transform.name + " Handle...Complete()");
+            // Debug.Log(transform.name + " Handle...Complete()");
 
-            this.cameraPosition = cameraPosition;
-            this.cameraRotation = cameraRotation;
-
-            if (sim) sim.Resume();
-
-            if (insetCamera)
+            if (sim)
             {
-                Debug.Log(transform.name + " show InsetCamera ? " + showInsetCamera);
-                insetCamera.gameObject.SetActive(showInsetCamera);
+                sim.Reset(cameraPosition, cameraRotation);
+                sim.Resume();
             }
+
+            if (fireButton) fireButton.interactable = true;
         }
+
+        this.cameraPosition = cameraPosition;
+        this.cameraRotation = cameraRotation;
     }
 }
